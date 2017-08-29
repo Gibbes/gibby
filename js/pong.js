5d3ed9d276dab6a2c0f04bca9f731cadba1b8bdf
Michael Gibbes <mgibbes@berkeley.edu>
2017-08-28 19:00:10 -0700

    Add bricks

--- js/pong.js
+++ js/pong.js
@@ -38,6 +38,21 @@ var paddleWidth = 10;
 var paddleHeight = 75;
 var paddle1Y = (canvas.height - paddleHeight) / 2;
 var paddle2Y = (canvas.height - paddleHeight) / 2;
+var brickRowCount = 5;
+var brickColumnCount = 5;
+var brickWidth = 20;
+var brickHeight = 75;
+var brickPadding = 10;
+var brickOffsetTop = (canvas.height / 2) - (brickHeight + brickPadding) * (brickRowCount / 2) + (brickPadding / 2);
+var brickOffsetLeft = (canvas.width / 2) - (brickWidth + brickPadding) * (brickColumnCount / 2) + (brickPadding / 2);
+var bricks = [];
+for (var c = 0; c < brickColumnCount; c++) {
+	bricks[c] = [];
+	for (var r = 0; r < brickRowCount; r++) {
+		bricks[c][r] = [0, 0];
+	}
+}
+
 /** DRAW FUNCTIONS */
 function drawBall() {
 	"use strict";
@@ -57,11 +72,29 @@ function drawPaddle(x, y) {
 	ctx.fill();
 	ctx.closePath();
 }
+function drawBricks() {
+	"use strict";
+	
+	for (var c = 0; c < brickColumnCount; c++) {
+		for (var r = 0; r < brickRowCount; r++) {
+			var brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+			var brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+			bricks[c][r][0] = brickX;
+			bricks[c][r][1] = brickY;
+			ctx.beginPath();
+			ctx.rect(brickX, brickY, brickWidth, brickHeight);
+			ctx.fillStyle = "#ff7630";
+			ctx.fill();
+			ctx.closePath();
+		}
+	}
+}
 function draw() {
 	"use strict";
 	
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	drawBall();
+	drawBricks();
 	drawPaddle(0, paddle1Y);
 	drawPaddle(canvas.width - paddleWidth, paddle2Y);
 	
